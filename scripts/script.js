d57a5494b5189b59f5726d7bf7d2570e1bc8ff3e
Katarzyna Werner <katarzyna.werner1234@gmail.com>
2025-04-22 13:11:31 +0200

    initial commit

---
+++ scripts/script.js
@@ -0,0 +1,10 @@
+(() => {
+  const swiper = new Swiper('#es-rc .es-swiper', {
+    loop: true,
+    autoHeight: true,
+  autoplay: {
+      delay: 2500,
+      disableOnInteraction: true,
+    }
+  })
+})()
